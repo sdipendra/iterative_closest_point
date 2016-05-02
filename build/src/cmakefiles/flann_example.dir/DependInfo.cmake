3807d276ebbbac0e468299098cef890f2da311bf
sdipendra <dipendra@iitk.ac.in>
2016-05-02 10:21:58 +0530

    cmake buildsystem and flann integration

---
+++ build/src/cmakefiles/flann_example.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dipendra/Dropbox/icp/iterative_closest_point/src/flann_example.cpp" "/home/dipendra/Dropbox/icp/iterative_closest_point/build/src/CMakeFiles/flann_example.dir/flann_example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include"
+  "/usr/include/hdf5/serial"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
