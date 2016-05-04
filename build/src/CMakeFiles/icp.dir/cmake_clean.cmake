4fa75da2dcfebe61af7b4f5d0df9b0c2297f359b
sdipendra <dipendra@iitk.ac.in>
2016-05-04 13:08:24 +0530

    running icp using flann kdtree

---
+++ build/src/CMakeFiles/icp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/icp.dir/icp.cpp.o"
+  "../bin/icp.pdb"
+  "../bin/icp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/icp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- build/src/cmakefiles/icp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for icp.
-# This may be replaced when dependencies are built.
