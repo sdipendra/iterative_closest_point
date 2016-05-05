d06c37ffe2c7d4d055b9da4eee2b64c97e63717a
sdipendra <dipendra@iitk.ac.in>
2016-05-05 20:53:17 +0530

    icp2 parrallel implementation using cuda completed and results are 50 times better in speed

--- src/cu/icp2.cu
+++ src/cu/icp2.cu
@@ -77,7 +77,7 @@ int main( int argc, char** argv)
 		
 		thrust::host_vector<float4> query_host(reading.size());
 		thrust::host_vector<float4> data_host(reference.size());	// cuda code
-				
+		
 		for(int i=0; i<reading.size(); ++i)	// copy point cloud data
 		{
 			query_host[i]=make_float4(reading[i][0], reading[i][1], reading[i][2], 0);	// cuda code
@@ -98,19 +98,36 @@ int main( int argc, char** argv)
 		flann::KDTreeCuda3dIndex<flann::L2_Simple<float> > index(data_device_matrix, index_params);
     index.buildIndex();
 
-		thrust::device_vector<int> indices_device(reading.size()*4);	// cuda code
-		thrust::device_vector<float> dists_device(reading.size()*4);
-		flann::Matrix<int> indices_device_matrix( (int*)thrust::raw_pointer_cast(&indices_device[0]),reading.size(),4);
-	flann::Matrix<float> dists_device_matrix( (float*)thrust::raw_pointer_cast(&dists_device[0]),reading.size(),4);
+		thrust::device_vector<int> indices_device(reading.size()*nn);	// cuda code
+		thrust::device_vector<float> dists_device(reading.size()*nn);
+		flann::Matrix<int> indices_device_matrix( (int*)thrust::raw_pointer_cast(&indices_device[0]),reading.size(),nn);
+	flann::Matrix<float> dists_device_matrix( (float*)thrust::raw_pointer_cast(&dists_device[0]),reading.size(),nn);
 		
 		flann::SearchParams sp;
 		sp.matrices_in_gpu_ram=true;
 				
 		int iterations = stoi(argv[3]);	// No. of icp iterations
 		
-		flann::Matrix<int> indices_host( new int[ reading.size()*4],reading.size(),4 );	// cuda code
-    flann::Matrix<float> dists_host( new float[ reading.size()*4],reading.size(),4 );
-				
+		flann::Matrix<int> indices_host( new int[ reading.size()*nn],reading.size(),nn );	// cuda code
+    flann::Matrix<float> dists_host( new float[ reading.size()*nn],reading.size(),nn );
+		
+		gsl_matrix *TF = gsl_matrix_alloc(4, 4);
+		gsl_vector *query_mean = gsl_vector_alloc(3);
+		gsl_vector *dataset_mean = gsl_vector_alloc(3);
+		gsl_matrix *U = gsl_matrix_alloc(3, 3);
+		gsl_matrix *V = gsl_matrix_alloc(3, 3);
+		gsl_vector *S = gsl_vector_alloc(3);
+		gsl_vector *work = gsl_vector_alloc(3);
+		gsl_matrix *R = gsl_matrix_alloc(3, 3);
+		gsl_vector *t = gsl_vector_alloc(3);
+		gsl_matrix *T = gsl_matrix_alloc(4, 4);
+		gsl_vector *temp1 = gsl_vector_alloc(3);
+		gsl_matrix *temp2 = gsl_matrix_alloc(4, 4);
+		gsl_vector *temp3 = gsl_vector_alloc(4);
+		gsl_vector *temp4 = gsl_vector_alloc(4);
+		
+		gsl_matrix_set_identity(TF);
+		
 		for(int i=0; i<iterations; ++i)
 		{
 	    index.knnSearch(query_device_matrix, indices_device_matrix, dists_device_matrix, nn, sp );	//cuda code
@@ -118,10 +135,88 @@ int main( int argc, char** argv)
     	thrust::copy( dists_device.begin(), dists_device.end(), dists_host.ptr() );	// cuda code
 			thrust::copy( indices_device.begin(), indices_device.end(), indices_host.ptr() );
 			
+			gsl_vector_set_zero(query_mean);
 			for(int j=0; j<reading.size(); ++j)
 			{
-//				cout<<query_host[j].x<<"  "<<query_host[j].y<<"  "<<query_host[j].z<<"  -  "<<indices_host[j][0]<<"  -  "<<data_host[indices_host[j][0]].x<<"  "<<data_host[indices_host[j][0]].y<<"  "<<data_host[indices_host[j][0]].z<<"  -  "<<dists_host[j][0]<<endl;
+				gsl_vector_set(query_mean, 0, gsl_vector_get(query_mean, 0)+query_host[j].x/reading.size());
+				gsl_vector_set(query_mean, 1, gsl_vector_get(query_mean, 1)+query_host[j].y/reading.size());
+				gsl_vector_set(query_mean, 2, gsl_vector_get(query_mean, 2)+query_host[j].z/reading.size());
 			}
+			
+			gsl_vector_set_zero(dataset_mean);
+			for(int j=0; j<reference.size(); ++j)
+			{
+				gsl_vector_set(dataset_mean, 0, gsl_vector_get(dataset_mean, 0)+data_host[j].x/reading.size());
+				gsl_vector_set(dataset_mean, 1, gsl_vector_get(dataset_mean, 1)+data_host[j].y/reading.size());
+				gsl_vector_set(dataset_mean, 2, gsl_vector_get(dataset_mean, 2)+data_host[j].z/reading.size());
+			}
+			
+			gsl_matrix_set_zero(U);
+			for(int j=0; j<reading.size(); ++j)
+			{
+				gsl_matrix_set(U, 0, 0, gsl_matrix_get(U, 0, 0)+(data_host[indices_host[j][0]].x-gsl_vector_get(dataset_mean, 0))*(query_host[j].x-gsl_vector_get(query_mean, 0)));
+				gsl_matrix_set(U, 0, 1, gsl_matrix_get(U, 0, 1)+(data_host[indices_host[j][0]].x-gsl_vector_get(dataset_mean, 0))*(query_host[j].y-gsl_vector_get(query_mean, 1)));
+				gsl_matrix_set(U, 0, 2, gsl_matrix_get(U, 0, 2)+(data_host[indices_host[j][0]].x-gsl_vector_get(dataset_mean, 0))*(query_host[j].z-gsl_vector_get(query_mean, 2)));
+				gsl_matrix_set(U, 1, 0, gsl_matrix_get(U, 1, 0)+(data_host[indices_host[j][0]].y-gsl_vector_get(dataset_mean, 1))*(query_host[j].x-gsl_vector_get(query_mean, 0)));
+				gsl_matrix_set(U, 1, 1, gsl_matrix_get(U, 1, 1)+(data_host[indices_host[j][0]].y-gsl_vector_get(dataset_mean, 1))*(query_host[j].y-gsl_vector_get(query_mean, 1)));
+				gsl_matrix_set(U, 1, 2, gsl_matrix_get(U, 1, 2)+(data_host[indices_host[j][0]].y-gsl_vector_get(dataset_mean, 1))*(query_host[j].z-gsl_vector_get(query_mean, 2)));
+				gsl_matrix_set(U, 2, 0, gsl_matrix_get(U, 2, 0)+(data_host[indices_host[j][0]].z-gsl_vector_get(dataset_mean, 2))*(query_host[j].x-gsl_vector_get(query_mean, 0)));
+				gsl_matrix_set(U, 2, 1, gsl_matrix_get(U, 2, 1)+(data_host[indices_host[j][0]].z-gsl_vector_get(dataset_mean, 2))*(query_host[j].y-gsl_vector_get(query_mean, 1)));
+				gsl_matrix_set(U, 2, 2, gsl_matrix_get(U, 2, 2)+(data_host[indices_host[j][0]].z-gsl_vector_get(dataset_mean, 2))*(query_host[j].z-gsl_vector_get(query_mean, 2)));
+			}
+				
+			
+			gsl_linalg_SV_decomp(U, V, S, work);
+			gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, U, V, 0.0, R);
+
+			gsl_blas_dgemv(CblasNoTrans, 1.0, R, query_mean, 0.0, temp1);
+			gsl_vector_memcpy(t, dataset_mean);
+			gsl_vector_sub(t, temp1);
+			
+			gsl_matrix_set_identity(T);
+			for(int j=0; j<3; ++j)
+			{
+				for(int k=0; k<3; k++)
+				{
+					gsl_matrix_set(T, j, k, gsl_matrix_get(R, j, k));
+				}
+				gsl_matrix_set(T, j, 3, gsl_vector_get(t, j));
+			}
+			
+			for(int j=0; j<reading.size(); ++j)
+			{
+				gsl_vector_set(temp3, 0, query_host[j].x);
+				gsl_vector_set(temp3, 1, query_host[j].y);
+				gsl_vector_set(temp3, 2, query_host[j].z);
+				gsl_vector_set(temp3, 3, 1.0);
+
+				gsl_blas_dgemv(CblasNoTrans, 1.0, T, temp3, 0.0, temp4);
+
+				query_host[j].x = gsl_vector_get(temp4, 0);
+				query_host[j].y = gsl_vector_get(temp4, 1);
+				query_host[j].z = gsl_vector_get(temp4, 2);
+			}
+			
+			gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, T, TF, 0.0, temp2);
+			gsl_matrix_memcpy(TF, temp2);
+			/*
+			cout<<endl<<"iteration - "<<i+1<<endl;
+			for(int j=0; j<4; ++j)
+			{
+				for(int k=0; k<4; ++k)
+				{
+					cout<<gsl_matrix_get(TF, j, k)<<"     ";
+				}
+				cout<<endl;
+			}
+			*/
+			
+/*			
+			for(int j=0; j<reading.size(); ++j)
+			{
+				cout<<query_host[j].x<<"  "<<query_host[j].y<<"  "<<query_host[j].z<<"  "<<query_host[j].w<<"  -  "<<indices_host[j][0]<<"  -  "<<data_host[indices_host[j][0]].x<<"  "<<data_host[indices_host[j][0]].y<<"  "<<data_host[indices_host[j][0]].z<<"  "<<data_host[indices_host[j][0]].w<<"  -  "<<dists_host[j][0]<<endl;
+			}
+			*/
 		}
 	}
 	return 0;
